da90615e1f0e62ad92b67e57a4e3905d86fd71ce
ilaydagurbak <ilaydagurbak@gmail.com>
2018-08-11 13:22:12 +0300

    local storage

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ class IndecisionApp extends React.Component {
       this.handleAddOption = this.handleAddOption.bind(this)
       this.handleDeleteOption = this.handleDeleteOption.bind(this)
       this.state = {
-        options: props.options
+        options: []
       }
     }
     componentDidMount() {
@@ -78,11 +78,7 @@ class IndecisionApp extends React.Component {
       )
     }
   }
-  
-  IndecisionApp.defaultProps = {
-    options: []
-  }
-  
+ 
   const Header = (props) => {
     return (
       <div>
